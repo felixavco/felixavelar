49bebcd2b2ef1fdb8bfebddcc0531f702f68c925
Felix Avelar <felixavco@gmail.com>
2019-05-08 10:21:03 -0600

    add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import './styles/App.css';
 //React Router
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Landing from './components/landing/Landing';
 import Portfolio from './components/portfolio/Portfolio';
 import Contact from './components/contact/Contact';
+import NotFound from './components/404/NotFound';
 
 class App extends Component {
 	render() {
@@ -19,6 +20,9 @@ class App extends Component {
 							<Route exact path="/" component={Landing} />
 							<Route exact path="/portfolio" component={Portfolio} />
 							<Route exact path="/contact" component={Contact} />
+							{/* CatchAll route  404 page */}
+							<Route exact path="/not-found" component={NotFound} />
+							<Redirect to="/not-found" />
 						</Switch>
 					</div>
 					<Footer />
---
+++ src/components/404/NotFound.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import NotFoundImg from '../../img/NotFound.gif';
+
+const NotFound = () => {
+	return (
+		<div className="container pt-5">
+			<div className="row">
+				<div className="col-md-6">
+					<img src={NotFoundImg} alt="Not Found" />
+				</div>
+				<div className="col-md-6 pt-5">
+					<h1 className="display-1 text-center mt-5">
+						404 <i class="far fa-frown-open" />
+					</h1>
+					<h3 className="display-4 text-center my-3">PAGE NOT FOUND</h3>
+          <h5 className="text-center"><Link to="/">Back to Home</Link></h5>
+					<div className="text-center" />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default NotFound;
