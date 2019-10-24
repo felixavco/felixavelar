c5504034fb73e5ae8acd88d81dc963804dee6f14
Felix Avelar <hey@felixavelar.com>
2019-10-23 21:36:08 -0600

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ export default class Navbar extends Component {
 		animateLinks: false,
 		menu: [
 			{ name: 'Home', path: '/' },
-			{ name: 'Portfolio', path: '/portfolio' },
+// 			{ name: 'Portfolio', path: '/portfolio' },
 			{ name: 'Contact', path: '/contact' }
 		]
 	};
